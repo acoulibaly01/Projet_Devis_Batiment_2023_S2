5f605fce52174e9d2b2d4cd5da330348c75e668c
acoulibaly01 <amadou.coulibaly@insa-strasbourg.fr>
2022-05-12 12:24:15 +0200

    AC ClasseA

---
+++ Projet_Groupe_7/src/main/java/fr/insa/monprojet/projet_groupe_7/SystemInfo.java
@@ -0,0 +1,13 @@
+package fr.insa.monprojet.projet_groupe_7;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ Projet_Groupe_7/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module fr.insa.monprojet.projet_groupe_7 {
+    requires javafx.controls;
+    exports fr.insa.monprojet.projet_groupe_7;
+}
