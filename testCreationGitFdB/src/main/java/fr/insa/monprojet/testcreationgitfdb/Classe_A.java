ac5ea4e9d7982ac36f7ad29a466d8292c84d042d
acoulibaly01 <amadou.coulibaly@insa-strasbourg.fr>
2022-05-12 11:18:20 +0200

    AC ClasseA

---
+++ testCreationGitFdB/src/main/java/fr/insa/monprojet/testcreationgitfdb/Classe_A.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.monprojet.testcreationgitfdb;
+
+/**
+ *
+ * @author Amadou Coulibaly
+ */
+public class Classe_A {
+    int x;
+    
+}
