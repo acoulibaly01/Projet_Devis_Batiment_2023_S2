6a26acaf1714df68b689fa6ad931baa639c35243
acoulibaly01 <amadou.coulibaly@insa-strasbourg.fr>
2022-05-12 12:42:36 +0200

    AC Barre

---
+++ Projet_Groupe_7/src/main/java/fr/insa/monprojet/projet_groupe_7/Laure_Barre.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.monprojet.projet_groupe_7;
+
+/**
+ *
+ * @author Amadou Coulibaly
+ */
+public class Laure_Barre {
+    int x;
+}
