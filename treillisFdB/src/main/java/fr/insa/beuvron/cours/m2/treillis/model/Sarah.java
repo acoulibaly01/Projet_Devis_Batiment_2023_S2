24d45445e3a0db5eb2c14852ab19372073cce377
acoulibaly01 <amadou.coulibaly@insa-strasbourg.fr>
2022-05-18 09:50:11 +0200

    AC Barre

---
+++ treillisFdB/src/main/java/fr/insa/beuvron/cours/m2/treillis/model/Sarah.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.beuvron.cours.m2.treillis.model;
+
+/**
+ *
+ * @author Amadou Coulibaly
+ */
+public class Sarah {
+    
+}
